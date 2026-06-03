0952312e08e9db438be87a407f9f85fe5a53d7fd
akramnemri <florensiahiro1@gmail.com>
2026-06-04 00:01:42 +0200

    ci: fix Node 20 deprecation and frontend test failure

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,14 +1,15 @@
 const { override, addWebpackModuleRule } = require('customize-cra');
+const path = require('path');
 
 module.exports = override(
-  addWebpackModuleRule({
-    test: /\.m?js/,
-    resolve: {
-      fullySpecified: false, // Disable strict ESM module resolution
-    },
-  }),
+  (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.fullySpecified = false;
+    return config;
+  },
   addWebpackModuleRule({
     test: /\.(js|jsx)$/,
+    include: path.resolve(__dirname, 'src'),
     exclude: /node_modules/,
     use: {
       loader: 'babel-loader',
